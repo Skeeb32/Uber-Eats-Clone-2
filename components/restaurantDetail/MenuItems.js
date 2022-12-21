995b914b0e72558f025d7ef25e8e24f49f74da6a
Skeeb32 <skeeb32@gmail.com>
2022-12-21 01:10:33 -0600

    Redux

--- components/restaurantDetail/MenuItems.js
+++ components/restaurantDetail/MenuItems.js
@@ -2,6 +2,7 @@ import { View, Text, StyleSheet, Image, ScrollView } from 'react-native'
 import React from 'react'
 import { Divider } from 'react-native-elements/dist/divider/Divider';
 import BouncyCheckbox from 'react-native-bouncy-checkbox';
+import { useDispatch } from 'react-redux';
 
 const foods = [
 
@@ -38,6 +39,7 @@ const foods = [
   
 ]; 
 
+
 const styles = StyleSheet.create({
   menuItemStyle: {
     flexDirection: "row",
@@ -53,7 +55,19 @@ const styles = StyleSheet.create({
 
 
 
-export default function MenuItems() {
+export default function MenuItems(restaurantName) {
+  const dispatch = useDispatch();
+
+  const selectItem = (item, checkboxValue) =>
+    dispatch({
+      type: "ADD_TO_CART",
+      payload: {
+        ...item,
+        restaurantName: restaurantName,
+        checkboxValue: checkboxValue,
+      },
+    });
+
   return (
     <ScrollView showsVerticalScrollIndicator={false}> 
     {foods.map((food, index) => (
@@ -62,6 +76,7 @@ export default function MenuItems() {
           <BouncyCheckbox
             iconStyle={{ borderColor: "lightgray", borderRadius: 0 }}
             fillColor="green"
+            onPress={(checkboxValue) => selectItem( food, checkboxValue )}
           />
           <FoodInfo food={food} />
           <FoodImage food={food} />
